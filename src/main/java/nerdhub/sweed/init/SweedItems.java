94757cd34857d413682e7cece44e1f7f4b867f03
Will Toll <will@jtoll.com>
2019-12-21 13:06:46 -0700

    Reverted the addition of Demeter to build file
    Removed Demeter from the build file because Sweed no longer extends certain Demeter classes when installed

--- src/main/java/nerdhub/sweed/init/SweedItems.java
+++ src/main/java/nerdhub/sweed/init/SweedItems.java
@@ -5,5 +5,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 
 public class SweedItems {
+
     public static final Item SWEED_SEEDS = new AliasedBlockItem(SweedBlocks.SWEED, new Item.Settings().group(ItemGroup.MATERIALS));
 }
